0e65258c2cac1818ef1d719bc4c2fd1c2887fd4d
Nikita Akmaikin <NikitaAkmaikin-F@yandex.ru>
2024-10-01 14:37:56 +0500

    Преход базы данных SQLite на MySQL

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',  // Файл базы данных SQLite
+const db = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
   logging: false,  // Отключение логов SQL-запросов
 });
 
