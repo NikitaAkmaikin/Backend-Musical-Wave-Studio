9dc7048f72516b7e4d38efef62ac1b95219d5a85
Nikita Akmaikin <NikitaAkmaikin-F@yandex.ru>
2024-10-06 19:33:26 +0500

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');  // Импортируем CORS
+const express = require('express'); 
+const cors = require('cors');
 require('dotenv').config();
 const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -12,7 +12,7 @@ const app = express();
 
 // Настройка CORS
 app.use(cors({
-  origin: 'http://localhost:5173',  // Указываем домен фронтенда
+  origin: 'http://62.113.110.34',  // Указываем домен фронтенда
 }));
 
 // Middleware для обработки JSON
@@ -20,6 +20,11 @@ app.use(express.json());
 
 // Обслуживание статических файлов (загруженные изображения)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Маршрут для проверки работоспособности сервера
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'Server is running' });
+});
 
 // Подключение маршрутов
 app.use('/api/auth', authRoutes);
@@ -30,6 +35,9 @@ app.use('/api', contactRoutes);
 // Подключение к базе данных и запуск сервера
 (async () => {
   try {
+    await db.authenticate();  // Проверка подключения к базе данных
+    console.log('Успешное подключение к базе данных');
+    
     await db.sync({ alter: true });
     console.log('База данных синхронизирована');
     
